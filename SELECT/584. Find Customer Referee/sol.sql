598b849673e7351812004b556274dcc547bf91cd
nikhil2060 <kumarnikhil2060@gmail.com>
2024-07-23 21:25:18 +0530

    first

---
+++ SELECT/584. Find Customer Referee/sol.sql
@@ -0,0 +1,6 @@
+
+-- Find the names of the customer that are not referred by the customer with id = 2.
+-- Return the result table in any order.
+
+SELECT name FROM Customer
+WHERE referee_id IS NULL OR referee_id != 2 
